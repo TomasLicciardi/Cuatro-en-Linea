21b353da6b3cee7d597c7f2b624d201b1562cfb4
TomasLicciardi <t.licciardi@alumno.um.edu.ar>
2022-10-12 12:16:49 -0300

    CuatroEnLinea completo con CircleCI y CodeClimate

---
+++ prueba.py
@@ -0,0 +1,56 @@
+# import pygame
+# lista1 = []
+# lista2 = []
+# for i in range(8):
+#     i = " "
+#     lista2.append(i)
+#     lista1.append(lista2)
+# print(lista1)
+
+# lista = [['x', 'x', 'x', 'x', " ", 'f ', 'g ', 'h '], ['b ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], ['c ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
+# ['d ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], ['d ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], ['d ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], ['h', ' ', ' ', ' ', ' ', ' ', ' ', ' ']] 
+# fila_0 = 0
+# fila_1 = 1
+# fila_2 = 2
+# fila_3 = 3
+# pasar_filas = 0
+# while True:
+#     for index in [pasar_filas]:
+#         if lista[pasar_filas][fila_0] == "x" and lista[pasar_filas][fila_1] == "x" and lista[pasar_filas][fila_2] == "x" and lista[pasar_filas][fila_3] == "x":
+#             break
+                
+#         if lista[pasar_filas][fila_0] == "o" and lista[pasar_filas][fila_1] == "o" and lista[pasar_filas][fila_2] == "o" and lista[pasar_filas][fila_3] == "o":
+#             break
+
+#         if fila_3 == 8:
+#             break
+                
+#         fila_0 += 1
+#         fila_1 += 1
+#         fila_2 += 1
+#         fila_3 += 1
+#         pasar_filas += 1
+#     if True:
+#         print(lista)
+#         break
+
+# for i in lista:
+    # print(i)
+
+# for i in reversed(lista[0]):
+    # print(i)
+
+# resultado = 0
+# a = True
+# b = False
+# while resultado != 10:
+#     if a == True:
+#         print("hola A")
+#         a = False
+#         b = True
+#         resultado += 1
+#     if b == True:
+#         print("Hola B")
+#         a = True
+#         b = False
+#         resultado += 0
